12194aec6e9cbb27503704d1618dfcbb9cab0fa0
Emanuel Trandafir <emanueltrandafir1993@gmail.com>
2026-01-11 19:31:05 +0200

    maven wrapper

--- order-service/src/main/java/com/etrandafir/asyncapi/orderservice/OrderController.java
+++ order-service/src/main/java/com/etrandafir/asyncapi/orderservice/OrderController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.etrandafir.asyncapi.contracts.events.OrderCreated;
 
+import io.github.springwolf.bindings.kafka.annotations.KafkaAsyncOperationBinding;
 import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
 import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher;
 
@@ -27,12 +29,6 @@ class OrderController {
     }
 
     @PostMapping
-    @AsyncPublisher(
-        operation = @AsyncOperation(
-            channelName = "order-created",
-            description = "Publishes an OrderCreated event when a new order is created"
-        )
-    )
     public ResponseEntity<OrderCreatedResponse> createOrder(@RequestBody CreateOrderRequest request) {
         String orderId = UUID.randomUUID().toString();
 
@@ -41,11 +37,22 @@ class OrderController {
             .setItems(request.items())
             .build();
 
-        streamBridge.send("orderCreated-out-0", event);
-
+        snedKafkaMessage(event);
         System.out.println("Published OrderCreated event: " + event);
+
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(new OrderCreatedResponse(orderId, "Order created successfully"));
+    }
+
+    @AsyncPublisher(
+        operation = @AsyncOperation(
+            channelName = "order-created",
+            description = "Publishes an OrderCreated event when a new order is created"
+        )
+    )
+    @KafkaAsyncOperationBinding
+    private void snedKafkaMessage(@Payload OrderCreated message) {
+        streamBridge.send("orderCreated-out-0", message);
     }
 
     public record CreateOrderRequest(Map<String, Integer> items) {}
